08f9ffba0e180f7d76ae0f6f8cd4c2dc14cc3202
dainiuxt <dainiuxt@gmail.com>
2021-10-25 21:48:40 +0300

    add range element

--- create-elements/script.js
+++ create-elements/script.js
@@ -1,50 +1,59 @@
-// create <div class="container"> and add it to <body>
-const container = document.createElement("div");
+const newLine = document.createElement("br");
 
+const container = document.createElement("div");
 container.classList.add("container");
 container.textContent = "Site body created with DOM only";
+
 document.body.appendChild(container);
 
-// create <div class="buttons"> add it to .container
 const buttons = document.createElement("div");
+const addButton = document.createElement("button");
+
+addButton.textContent = "Create new input!";
 
 container.appendChild(buttons);
 
-// create <div class="results" and add it to .container
+let counter = 0;
+
+const myRange = document.createElement("input");
+myRange.setAttribute("type", "range");
+myRange.setAttribute("max", 10);
+myRange.setAttribute("value", 0);
+
+buttons.appendChild(addButton);
+buttons.appendChild(newLine);
+buttons.appendChild(myRange);
+
 const results = document.createElement("div");
 
 results.classList.add("results");
 results.textContent = "Here will go results:";
+
 container.appendChild(results);
 
-// create button and text input, add them to .buttons
-const addInput = document.createElement("input")
-
-addInput.setAttribute("type", "text")
-addInput.classList.add("inputbox");
-
-const addButton = document.createElement("button");
-
-addButton.classList.add("left");
-addButton.textContent = "Press me!";
-
 addButton.onclick = function() {
-    let entry = document.querySelector(".inputbox").value;
-    let p = document.createElement('p');
-    let deleteRecord = document.createElement("button");
+    if (counter < 10) {
+        const addInput = document.createElement("input")
+        addInput.setAttribute("type", "text")
+        addInput.classList.add("inputbox");
+        addInput.setAttribute("placeholder", "Start typing...");
+        
+        const p = document.createElement('p');
+        const deleteRecord = document.createElement("button");
 
-    deleteRecord.classList.add("remover");
-    deleteRecord.textContent = "Delete this";
-    deleteRecord.onclick = function() {
-        let selected = deleteRecord.parentElement;
-        selected.remove();
-    }
-    p.textContent = entry;
-    p.appendChild(deleteRecord);
-    console.log()
-    results.appendChild(p);
-    document.querySelector(".inputbox").value = "";
+        deleteRecord.classList.add("remover");
+        deleteRecord.textContent = "Delete this";
+        deleteRecord.onclick = function() {
+            let selected = deleteRecord.parentElement;
+            selected.remove();
+        }
+        
+        p.appendChild(addInput);
+        p.appendChild(deleteRecord);
+        results.append(p);
+        addInput.focus();
+        counter++;
+        myRange.setAttribute("value", counter);
+        console.log(counter);
+    } else {alert('That\'s too much!');}
 };
-
-buttons.appendChild(addInput);
-buttons.appendChild(addButton);
